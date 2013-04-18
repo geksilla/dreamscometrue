52a27491462ec82ba414a0ccd16411dcbd6fee78
Denys <geksilla@gmail.com>
2013-04-18 22:35:55 +0300

    fb like event

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 function likeAnimation()
 {
-	
+	$('#mask').animate({"top":400},1000,'easeInOutBounce');
+	$('#masked').animate({"top":-400},1000,'easeInOutBounce');
 }
 
 function unlikeAnimation()
 {
-	
+	$('#mask').animate({"top":450},1000,'easeInOutBounce');
+	$('#masked').animate({"top":-450},1000,'easeInOutBounce');	
 }
